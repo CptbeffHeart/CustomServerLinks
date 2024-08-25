8395ddeea0789efdcc8d1087344dec21fecdae33
CptbeffHeart <51067790+CptbeffHeart@users.noreply.github.com>
2024-08-25 03:34:55 +0200

    feat: Add customserverlink reload command (#4)

---
+++ src/main/java/com/expectale/customserverlinks/command/ServerLinksReloadCommand.java
@@ -0,0 +1,29 @@
+package com.expectale.customserverlinks.command;
+
+import com.expectale.customserverlinks.CustomServerLinks;
+import com.expectale.customserverlinks.serverlinks.ServerLinkManager;
+import net.kyori.adventure.text.Component;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ServerLinksReloadCommand implements TabExecutor {
+    
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 0 || !args[0].equalsIgnoreCase("reload")) return false;
+        CustomServerLinks.INSTANCE.reloadConfig();
+        ServerLinkManager.reloadLinks();
+        sender.sendMessage(Component.text("§a[!] CustomServerLinks config have been reloaded"));
+        return true;
+    }
+    
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return List.of("reload");
+    }
+}
